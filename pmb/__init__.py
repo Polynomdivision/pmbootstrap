090fe8d8f6d8c5c461b39f07fba8d0c9cdbb1a3f
Alexander Polynomdivision <digitalmail555@googlemail.com>
2018-02-17 16:10:59 +0100

    Only check for binfmt_misc when needed

--- pmb/__init__.py
+++ pmb/__init__.py
@@ -39,7 +39,6 @@ def main():
     try:
         # Sanity checks
         other.check_grsec(args)
-        other.check_binfmt_misc(args)
         if not args.as_root and os.geteuid() == 0:
             raise RuntimeError("Do not run pmbootstrap as root!")
 
